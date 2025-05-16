39f3d4992d4398a0366b457602ac8b2734e7fea5
jaodograu <126591871+jaodograu@users.noreply.github.com>
2025-05-16 09:00:52 -0300

    Add files via upload

---
+++ BACKEND/atividade4.js
@@ -0,0 +1,24 @@
+class Carro{
+    constructor(modelo,cor){
+        this.modelo = modelo;
+        this.cor = cor;
+        this.velocidadeAtual = 0;
+    }
+    acelerar(valor){
+        this.velocidadeAtual += valor;
+        console.log(`${this.modelo}acelerou.velocidade atual:${this.velocidadeAtual}`);
+    }
+    frear(valor){
+        this.velocidadeAtual -= valor;
+        if(this.velocidadeAtual < 0){
+            this.velocidadeAtual = 0;
+        }
+        console.log(`Modelo ${this.modelo}, Freou, Cor ${this.cor}, Velocidade ${this.velocidadeAtual} Km/h`);
+    }
+    mostrarStatus(){
+        console.log(`Modelo: ${this.modelo}, Cor: ${this.cor}, Velocidade: ${thi.velocidadeAtual}`);
+    }
+}
+let carro = new Carro("Jetta, Preto ");
+//carro.mostrarStatus():
+carro.acelerar(283);
